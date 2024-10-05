25c6e0d961a04145bbbf2b1bd028f6909274bdf6
Jamaldin <thisisjoma@gmail.com>
2024-10-05 17:52:11 +0600

    ongladim

--- main.dart
+++ main.dart
@@ -2,7 +2,17 @@ import 'teacher.dart';
 
 void main() {
   Teacher teacher = Teacher(
-      name: 'John', lastName: 'Sim', age: 40, address: 'Osh', phone: '055123');
+      name: 'John2', lastName: 'Sim', age: 40, address: 'Osh', phone: '055123');
 
   teacher.fullName();
 }
+//git init
+//git add .
+//git commit -m "ozgarish"
+//git branch
+//git remote //////
+//git push
+
+//git add .
+//git commit -m "ozgarish"
+//git push
